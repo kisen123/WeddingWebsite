66005b08722cf9fd4cf2ea5e0e8c98a800e2b241
Kristian Mølbach Lian <kristian.lian@norconsult.com>
2026-03-23 20:26:35 +0000

    Removed obsolete commands

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -5,6 +5,4 @@ cd /workspaces/bryllup-nettside/frontend
 npm install
 
 cd /workspaces/bryllup-nettside
-sudo chown -R vscode:vscode /workspaces/bryllup-nettside
-sudo find . -name 'obj' -prune -exec rm -rf {} \;
 dotnet restore bryllup-nettside.sln
