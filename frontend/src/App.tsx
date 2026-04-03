36f49cf54545d2b1717813064263a90a3c460c62
Kristian Mølbach Lian <kristian.lian@norconsult.com>
2026-04-03 10:11:54 +0000

    Refactored webpage to have scrollview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,13 @@
 import './App.scss';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import { Header } from './components/Header/Header.tsx';
-import { ContactPage } from './pages/ContactPage.tsx';
-import { HomePage } from './pages/HomePage.tsx';
-import { InfoPage } from './pages/InfoPage.tsx';
-import { QaPage } from './pages/QaPage.tsx';
-import { RsvpPage } from './pages/RsvpPage.tsx';
+import { MainPage } from './components/MainPage/MainPage.tsx';
 
 export default function App() {
   return (
     <div className="wedding-app">
-      <BrowserRouter>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/info" element={<InfoPage />} />
-            <Route path="/rsvp" element={<RsvpPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/q-a" element={<QaPage />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+      <Header />
+      <MainPage />
     </div>
   );
 }
--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -1,9 +1,27 @@
+import { ContactPage } from '../../pages/ContactPage.tsx';
+import { InfoPage } from '../../pages/InfoPage.tsx';
+import { QaPage } from '../../pages/QaPage.tsx';
+import { RsvpPage } from '../../pages/RsvpPage.tsx';
 import { WeAreGettingMarried } from '../WeAreGettingMarried/WeAreGettingMarried.tsx';
 
 export function MainPage() {
   return (
     <main className="main-page">
-      <WeAreGettingMarried />
+      <section id="home">
+        <WeAreGettingMarried />
+      </section>
+      <section id="info">
+        <InfoPage />
+      </section>
+      <section id="rsvp">
+        <RsvpPage />
+      </section>
+      <section id="contact">
+        <ContactPage />
+      </section>
+      <section id="qa">
+        <QaPage />
+      </section>
     </main>
   );
 }
