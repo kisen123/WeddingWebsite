2d52fe8b3a651163144a581c79d83575db654c37
Kristian Mølbach Lian <kristian.lian@norconsult.com>
2026-04-03 10:14:09 +0000

    Reset URL route for each reload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.scss';
 
+import { useEffect } from 'react';
+
 import { Header } from './components/Header/Header.tsx';
 import { MainPage } from './components/MainPage/MainPage.tsx';
 
 export default function App() {
+  useEffect(() => {
+    window.history.replaceState(null, '', '/');
+  }, []);
+
   return (
     <div className="wedding-app">
       <Header />
